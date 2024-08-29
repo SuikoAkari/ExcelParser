101e4ba9e3fbfd01bbd652f188d7f536a928f039
AlessandroCH <alelo2006ch@gmail.com>
2024-08-29 21:12:29 +0200

    preparing for 5.0.50

--- ExcelParser/Program.cs
+++ ExcelParser/Program.cs
@@ -8,5 +8,5 @@ Parser parser = new Parser();
 string[] files = Directory.GetFiles("Data");
 foreach(string file in files)
 {
-   if(file.Contains("Affix"))parser.Parse(file);
+  parser.Parse(file);
 }
