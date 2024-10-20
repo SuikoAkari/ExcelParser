bdfeafe88bda6caf9ae9e0baa02f7d11f88e5568
AlessandroCH <zombiered93@gmail.com>
2024-10-20 17:33:22 +0200

    New configs

---
+++ ExcelParser/ActivityConf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelParser
+{
+    public class ActivityConf
+    {
+        public uint activityId;
+        public uint activityType;
+        public uint scheduleId;
+        public uint[] meetCondList;
+        public string beginTime;
+        public string endTime;
+    }
+    public class NewActivityExcelConfigData
+    {
+        public uint activityId;
+    }
+
+   
+}
--- ExcelParser/Program.cs
+++ ExcelParser/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using ExcelParser;
+using Newtonsoft.Json;
 using Pastel;
 
 Console.WriteLine("Starting parsing...");
@@ -9,8 +10,27 @@ Parser parser = new Parser();
 string[] files = Directory.GetFiles("Data");
 foreach(string file in files)
 {
-/*if(file.Contains("scene3"))*/parser.Parse(file);
+if(file.Contains("WeaponPromote"))parser.Parse(file);
 }
+
+
+/*List<NewActivityExcelConfigData> newActivities = JsonConvert.DeserializeObject<List<NewActivityExcelConfigData>>(File.ReadAllText("Output/NewActivityExcelConfigData.json"));
+List<ActivityConf> newConf = new List<ActivityConf>();
+
+foreach(NewActivityExcelConfigData config in newActivities)
+{
+    newConf.Add(new ActivityConf()
+    {
+        activityId=config.activityId,
+        activityType=0,
+        scheduleId=0,
+        meetCondList=new uint[0],
+        beginTime= "2024-08-26T00:00:00+02:00",
+        endTime= "2025-09-26T23:59:59+02:00"
+    });
+}
+File.WriteAllText("ActivityConfig.json", JsonConvert.SerializeObject(newConf, Formatting.Indented));*/
+
 
 /*string[] unknownFiles = Directory.GetFiles("D:\\Genshin5.1\\AssetMap\\Excels\\All\\MiHoYoBinData");
 int tot = 0;
